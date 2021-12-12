947011924751170c17bd6c249e4467d10a7331e5
Trish <trishedu12@gmail.com>
2021-12-13 02:30:40 +0530

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,46 @@
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
+import FooterCardContainer from "./FooterCardContainer";
+import Container from "../GlobalComponents/Container";
+
+const Footer = () => (
+  <footer css={styles} id="contact">
+    <FooterCardContainer />
+    <Container>
+      <p>
+        Copyright &copy;2021 All rights reserved | made with
+        <i className="far fa-heart"></i> by{" "}
+        <a
+          href="https://colorlib.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        ></a>
+        trish
+      </p>
+    </Container>
+  </footer>
+);
+
+const styles = css`
+  background: #000;
+  .container {
+    padding: 30px 0;
+    border-top: 1px solid #1a1a1a;
+    text-align: center;
+    p {
+      font-size: 15px;
+      color: #aab1b7;
+      a {
+        text-decoration: none;
+        color: #ff1414;
+      }
+    }
+  }
+  @media (max-width: 1200px) {
+    .container {
+      max-width: 90%;
+    }
+  }
+`;
+
+export default Footer;
